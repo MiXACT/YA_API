797e6f080b01eb88cb2b00c840d8af28c84e64bf
MiXACT <out_lawz@mail.ru>
2021-10-03 14:58:10 +0300

    File uploading

--- ya_api.py
+++ ya_api.py
@@ -1,16 +1,29 @@
+import requests
+
+
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
     def upload(self, file_path: str):
         """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': file_path}
+        resp = requests.get(upload_url, headers=headers, params=params)
+        url = resp.json().get('href', '')
+        requests.put(url=url, data=open('ya_test.txt', 'rb'))
 
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
+    path_to_file = 'ya_test.txt'
+    token = ''
     uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    uploader.upload(path_to_file)
